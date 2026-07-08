3c0bd51200b5b1895e1a925137c326f355e44fc1
homeless-coder <luisferpo13@hotmail.com>
2026-07-08 16:37:41 -0600

    feat(router): register route

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,6 @@
-import express from 'express';
-const app = express();
+import server from './server';
 const PORT = process.env.PORT || 3000;
 
-// Routing
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.get('/ecommerce', (req, res) => {
-  res.send('Este es el ecommerce');
-});
-
-
-app.get('/blog', (req, res) => {
-  res.send('Este es el blog');
-});
-
-app.get('/nosotros', (req, res) => {
-  res.send('Este es el about us');
-});
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
